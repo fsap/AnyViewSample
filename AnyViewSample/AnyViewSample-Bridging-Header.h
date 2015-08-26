6001d41d92e665c4ac283cc408011bfa8e4ede36
shuichi-fujiwara <shuichi.fujiwara@enish.jp>
2015-08-26 16:21:39 +0900

    AFNetworking導入

---
+++ AnyViewSample/AnyViewSample-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  AnyViewSample-Bridging-Header.h
+//  AnyViewSample
+//
+//  Created by 藤原修市 on 2015/08/26.
+//  Copyright (c) 2015年 fsap. All rights reserved.
+//
+
+#ifndef AnyViewSample_AnyViewSample_Bridging_Header_h
+#define AnyViewSample_AnyViewSample_Bridging_Header_h
+
+#endif
+
+#import <AFNetworking/AFNetworking.h>
+#import <AFNetworkActivityLogger/AFNetworkActivityLogger.h>
--- AnyViewSample/AppDelegate.swift
+++ AnyViewSample/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreData
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -17,6 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Network Logging
+        AFNetworkActivityLogger.sharedLogger().level = .AFLoggerLevelDebug
+        AFNetworkActivityLogger.sharedLogger().startLogging()
+        
         return true
     }
 
